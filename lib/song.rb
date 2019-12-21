d1c9ad54a454ce12e3dddb4a74846c6411a6f486
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-12-21 17:17:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,3 +15,12 @@ end
   end
 end
 
+drake = Artist.new("Drake")
+drake.add_song("In My Feelings")
+drake.add_song("Hotline Bling")
+
+kiki = Song.new("In My Feelings", "hip-hop")
+hotline = Song.new("Hotline Bling", "pop")
+ 
+drake.add_song(kiki)
+drake.add_song(hotline)
